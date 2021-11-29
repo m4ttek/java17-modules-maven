abcd8790ae525f760a4866e90afc2f45b4127b96
Marcin Chrost <marcinc@backbase.com>
2021-11-29 11:40:01 +0100

    Java 11 & 17 without modules

--- stacktrace/src/main/java/com/chrosciu/stacktrace/StacktraceService.java
+++ stacktrace/src/main/java/com/chrosciu/stacktrace/StacktraceService.java
@@ -1,20 +1,14 @@
 package com.chrosciu.stacktrace;
 
-import sun.reflect.Reflection;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StacktraceService {
     public List<String> getCallStackClassNames() {
-        List<String> clazzez = new ArrayList<>();
-        int i = 0;
-        Class<?> caller = Reflection.getCallerClass(i++);
-        do {
-            String clazz = caller.getName();
-            clazzez.add(clazz);
-            caller = Reflection.getCallerClass(i++);
-        } while (caller != null);
-        return clazzez;
+        return StackWalker
+                .getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+                .walk(stackFrameStream -> stackFrameStream
+                        .map(StackWalker.StackFrame::getClassName)
+                        .collect(Collectors.toList()));
     }
 }
